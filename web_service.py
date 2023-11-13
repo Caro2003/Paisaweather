30a6e0bbef9cc9d10be44bae38a2b69399deb5cb
Carolina27 <carolina.vargasv@upb.edu.co>
2023-11-13 11:41:51 -0500

    aplicacion web flask

---
+++ web_service.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, jsonify
+from waitress import serve
+import requests, os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/weather', methods=['GET'])
+def weather_route():
+    response = requests.get('http://weather:5000/weather')
+    weather_data = response.json()
+    return jsonify(weather_data)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8080))
+    serve(app, host='0.0.0.0', port=port)
